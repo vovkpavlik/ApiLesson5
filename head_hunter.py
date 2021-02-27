2e44aae29f41601f816a500d7984b1c790443bf2
vovkpavlik <vovk.pavlik@bk.ru>
2021-02-27 14:29:56 +0300

    Разделил функцию get_hh_stats. - создал функцию по сбору статистики для одного языка; - создал функцию по количествую вакансий для одного языка.

--- head_hunter.py
+++ head_hunter.py
@@ -21,31 +21,38 @@ def get_vacancies(lang, page):
     return response.json()
 
 
+def get_language_salaries(lang):
+    salaries = []
+    vacancies = []
+
+    response = get_vacancies(lang, 0)
+    vacancies += response["items"]
+    pages = response["pages"]
+
+    # Результатов не может быть более 2000(https://github.com/hhru/api/blob/master/docs/vacancies.md#запрос)
+    for page in range(1, pages):
+        vacancies += get_vacancies(lang, page)["items"]
+
+    for vacancy in vacancies:
+        if not vacancy["salary"] or vacancy["salary"]["currency"] != "RUR":
+            continue
+        if predicted_salary := predict_rub_salary(vacancy["salary"]["from"], vacancy["salary"]["to"]):
+            salaries.append(predicted_salary)
+    return salaries
+
+
+def get_language_vacancies(lang):
+    response = get_vacancies(lang, 0)
+    vacancies_found = response["found"]
+    return vacancies_found
+
 def get_hh_stats(languages):
     hh_stats = {}
     for lang in languages:
-        salaries = []
-        vacancies = []
-
-        response = get_vacancies(lang, 0)
-        vacancies_found = response["found"]
-        vacancies += response["items"]
-        pages = response["pages"]
-
-        # Результатов не может быть более 2000(https://github.com/hhru/api/blob/master/docs/vacancies.md#запрос)
-        for page in range(1, pages):
-            vacancies += get_vacancies(lang, page)["items"]
-
-        for vacancy in vacancies:
-            if not vacancy["salary"] or vacancy["salary"]["currency"] != "RUR":
-                continue
-            if predicted_salary := predict_rub_salary(vacancy["salary"]["from"], vacancy["salary"]["to"]):
-                salaries.append(predicted_salary)
-
         lang_stat = {
-            "vacancies_found": vacancies_found,
-            "vacancies_processed": len(salaries),
-            "average_salaries": int(sum(salaries) / len(salaries))
+            "vacancies_found": get_language_vacancies(lang),
+            "vacancies_processed": len(get_language_salaries(lang)),
+            "average_salaries": int(sum(get_language_salaries(lang)) / len(get_language_salaries(lang)))
         }
         hh_stats.update({lang: lang_stat})
     return hh_stats
